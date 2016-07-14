d67529b0cfcee565c67ea5fd04c4c45ffc5fcbe9
Richard Teammco <teammcr192@gmail.com>
2016-07-13 22:09:05 -0700

    Added basic image loading functionality.

--- source/super_resolution.cpp
+++ source/super_resolution.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
 #include "gflags/gflags.h"
 #include "glog/logging.h"
 
-DEFINE_string(
-    images_dir, "", "A directory containing the video frame images.");
+DEFINE_string(image_path, "", "The path to an image file.");
 
 int main(int argc, char** argv) {
+  google::SetUsageMessage(
+      "Super-resolves a video into a higher quality video.");
+  google::SetVersionString("0.1");
   gflags::ParseCommandLineFlags(&argc, &argv, true);
-  std::cout << "Hello, world" << std::endl;
+  google::InitGoogleLogging(argv[0]);
 
-  if (!FLAGS_images_dir.empty()) {
-    std::cout << "WIN" << std::endl;
-  } else {
-    std::cout << "FAIL" << std::endl;
-  }
+  CHECK(!FLAGS_image_path.empty()) << "Must provide an image file path.";
 
+  const cv::Mat image = cv::imread(FLAGS_image_path, CV_LOAD_IMAGE_GRAYSCALE);
+  cv::imshow("Image", image);
+  cv::waitKey(0);
+
+  // TODO(richard):
+  // 1. set up a blur kernel and convolve the image to blur it.
+  // 2. downsample the image into multiple frames with different motion offsets.
+  const int scale = 2;
+  cv::Mat blur_matrix;
   // 1. Verify that the data has 2N frames.
   // 2. Load up all images.
   // 3. Compute SR for the middle image.
