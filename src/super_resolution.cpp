0ab7215723b3354fc351592a9d82faa93e33450d
Richard Teammco <teammcr192@gmail.com>
2016-07-24 21:35:37 -0700

    Moved the data generator code into a new file.

--- src/super_resolution.cpp
+++ src/super_resolution.cpp
@@ -1,53 +1,7 @@
 #include <iostream>
-#include <vector>
-
-#include "./data_generator.h"
-
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-
-#include "gflags/gflags.h"
-#include "glog/logging.h"
-
-DEFINE_string(image_path, "", "The path to an image file.");
 
 int main(int argc, char** argv) {
-  google::SetUsageMessage(
-      "Super-resolves a video into a higher quality video.");
-  google::SetVersionString("0.1");
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
-  google::InitGoogleLogging(argv[0]);
-
-  CHECK(!FLAGS_image_path.empty()) << "Must provide an image file path.";
-
-  const cv::Mat image = cv::imread(FLAGS_image_path, CV_LOAD_IMAGE_GRAYSCALE);
-  super_resolution::DataGenerator data_generator(image);
-
-  cv::imshow("Image", image);
-  cv::waitKey(0);
-
-  data_generator.SetMotionSequence({
-      super_resolution::MotionShift(0, 0),
-      super_resolution::MotionShift(0, 1),
-      super_resolution::MotionShift(2, 0),
-      super_resolution::MotionShift(1, 2)});
-  std::vector<cv::Mat> low_res_images =
-      data_generator.GenerateLowResImages(5, 4);
-
-  cv::Mat vis;
-  cv::resize(low_res_images[0], vis, image.size());
-  cv::imshow("low res 1", vis);
-  cv::waitKey(0);
-
-  // TODO(richard): the list of algorithm steps (eventually).
-  // 1. Verify that the data has 2N frames.
-  // 2. Load up all images.
-  // 3. Compute SR for the middle image.
-  // 4. Evaluate the results.
-  // Ultimately, I/O is:
-  //  in  => one of my old low-quality videos
-  //  out => noticably better quality version of that video
+  std::cout << "Hello, world" << std::endl;
 
   return EXIT_SUCCESS;
 }
