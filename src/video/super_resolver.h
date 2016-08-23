f167c15e24a25a6808661d50233c7c61df4f9f9d
Richard Teammco <teammcr192@gmail.com>
2016-08-23 13:37:21 -0500

    Added variables to the SR object and options struct.
    Also comments.

--- src/video/super_resolver.h
+++ src/video/super_resolver.h
@@ -4,23 +4,44 @@
 #ifndef SRC_VIDEO_SUPER_RESOLVER_H_
 #define SRC_VIDEO_SUPER_RESOLVER_H_
 
+#include <string>
+
 #include "video/video_loader.h"
 
 namespace super_resolution {
 
+// All possible options for the super resolution algorithm.
 struct SuperResolutionOptions {
-  // TODO(richard): implement.
+  std::string super_resolution_algorithm = "";
+  std::string optical_flow_algorithm = "";
+
+  int scale = 2;
+  int num_iterations = 0;
+
+  double blur_kernel_size = 3;
+  double blur_sigma = 3;
+
+  bool use_gpu = false;
 };
 
 class SuperResolver {
  public:
   SuperResolver(
       const VideoLoader& video_loader,
-      const SuperResolutionOptions& options) {}
+      const SuperResolutionOptions& options) :
+  video_loader_(video_loader),
+  options_(options) {}
 
+  // Apply the super resolution algorithm to the given data (in the given
+  // VideoLoader). All parameters will be set according to the given
+  // SuperResolutionOptions.
   void SuperResolve();
 
  private:
+  // The VideoLoader object containing the low-resolution video frames.
+  const VideoLoader& video_loader_;
+
+  const SuperResolutionOptions& options_;
 };
 
 }  // namespace super_resolution
