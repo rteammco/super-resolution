5c5928ac0950caf700a17f4ed074667e0be9a860
Richard Teammco <teammcr192@gmail.com>
2016-08-29 13:40:09 -0500

    Updated data generator to save files.
    Also removed test code from shift_add_fusion.

--- src/generate_data.cpp
+++ src/generate_data.cpp
@@ -1,9 +1,11 @@
 // This binary is used to generate low-resolution images from a given
 // high-resolution ground truth image.
 
+#include <string>
 #include <vector>
 
 #include "data_generator/data_generator.h"
+#include "util/macros.h"
 #include "util/util.h"
 
 #include "opencv2/core/core.hpp"
@@ -13,8 +15,13 @@
 #include "gflags/gflags.h"
 #include "glog/logging.h"
 
-DEFINE_string(image_path, "",
+// Input and output files.
+DEFINE_string(input_image, "",
     "Path to the HR image that will be used to generate the LR images.");
+DEFINE_string(output_image_dir, "",
+    "Path to a directory that will contain all of the generated LR images.");
+
+// Parameters for the low-resolution image generation.
 DEFINE_bool(no_image_blur, false,
     "Option to not blur the generated LR images.");
 DEFINE_int32(noise_standard_deviation, 5,
@@ -28,13 +35,12 @@ int main(int argc, char** argv) {
   super_resolution::util::InitApp(argc, argv,
       "Generate low-resolution frames from a high-resolution image.");
 
-  CHECK(!FLAGS_image_path.empty()) << "Must provide an image file path.";
+  REQUIRE_ARG(FLAGS_input_image);
+  REQUIRE_ARG(FLAGS_output_image_dir);
 
-  const cv::Mat image = cv::imread(FLAGS_image_path, CV_LOAD_IMAGE_GRAYSCALE);
+  const cv::Mat image = cv::imread(
+      FLAGS_input_image, CV_LOAD_IMAGE_GRAYSCALE);
   super_resolution::DataGenerator data_generator(image);
-
-  cv::imshow("Image", image);
-  cv::waitKey(0);
 
   data_generator.SetMotionSequence({
       super_resolution::MotionShift(0, 0),
@@ -46,11 +52,15 @@ int main(int argc, char** argv) {
   std::vector<cv::Mat> low_res_images = data_generator.GenerateLowResImages(
       FLAGS_downsampling_scale, FLAGS_number_of_frames);
 
-  cv::Mat vis;
-  cv::resize(low_res_images[0], vis, image.size());
-  cv::imshow("low res 1", vis);
-  cv::imshow("low res 1 norm", low_res_images[0]);
-  cv::waitKey(0);
+  // Save the image to the provided directory.
+  for (int i = 0; i < low_res_images.size(); ++i) {
+    std::string image_path =
+        FLAGS_output_image_dir + "/low_res_" + std::to_string(i) + ".jpg";
+    // Convert to 0-255 scale before saving JPG file.
+    cv::Mat output_image = low_res_images[i];
+    output_image.convertTo(output_image, CV_8UC3, 255.0); 
+    cv::imwrite(image_path, output_image);
+  }
 
   return EXIT_SUCCESS;
 }
--- src/shift_add_fusion.cpp
+++ src/shift_add_fusion.cpp
@@ -3,7 +3,6 @@
 
 #include <string>
 
-#include "util/macros.h"
 #include "util/util.h"
 
 #include "gflags/gflags.h"
@@ -12,10 +11,6 @@
 int main(int argc, char** argv) {
   super_resolution::util::InitApp(argc, argv,
       "A trivial implementation of shift-add fusion.");
-
-  std::string FLAGS_my_str = "";
-
-  REQUIRE_ARG(FLAGS_my_str);
 
   return EXIT_SUCCESS;
 }
