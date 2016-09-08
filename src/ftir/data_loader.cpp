aa2817e00c3661e479f7467e501b6a75341c82a3
Richard Teammco <teammcr192@gmail.com>
2016-09-08 13:36:17 -0500

    Added a template loader class for FTIR data.

---
+++ src/ftir/data_loader.cpp
@@ -0,0 +1,7 @@
+#include "ftir/data_loader.h"
+
+namespace super_resolution {
+namespace ftir {
+
+}  // namespace ftir
+}  // namespace super_resolution
---
+++ src/ftir/data_loader.h
@@ -0,0 +1,17 @@
+// Provides an API to load and store FT-IR data sets.
+
+#ifndef SRC_FTIR_DATA_LOADER_H_
+#define SRC_FTIR_DATA_LOADER_H_
+
+namespace super_resolution {
+namespace ftir {
+
+class DataLoader {
+ public:
+ private:
+};
+
+}  // namespace ftir
+}  // namespace super_resolution
+
+#endif  // SRC_FTIR_DATA_LOADER_H_
