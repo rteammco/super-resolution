f292b1becd3b50f96cbef2198c3a2d8a9009947a
Richard Teammco <teammcr192@gmail.com>
2016-09-30 10:47:35 -0500

    Small update to MAP solver template file.

--- src/solvers/super_resolution_map.h
+++ src/solvers/super_resolution_map.h
@@ -1,9 +1,20 @@
 #ifndef SRC_SOLVERS_SUPER_RESOLUTION_MAP_H_
 #define SRC_SOLVERS_SUPER_RESOLUTION_MAP_H_
 
+#include <vector>
+
 namespace super_resolution {
 
-class MapSolver {
+class MapSolver : public Solver {
+ public:
+  MapSolver(
+      const std::vector<ImageData>& low_res_images,
+      const ImageModel& image_model);
+
+  virtual ImageData Solve();
+
+ private:
+  // here
 };
 
 }  // namespace super_resolution
