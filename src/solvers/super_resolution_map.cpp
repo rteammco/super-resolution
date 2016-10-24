a85045a91c60ed40786dd6987291848c69415750
Richard Teammco <teammcr192@gmail.com>
2016-10-24 16:48:55 -0500

    Added some tmp code into the MAP solver part.
    This is commented and needs to be implemented once I figure out which solver to use.

--- src/solvers/super_resolution_map.cpp
+++ src/solvers/super_resolution_map.cpp
@@ -1,13 +1,49 @@
 #include "solvers/super_resolution_map.h"
 
+#include <iostream>
+
 #include "image/image_data.h"
 
 namespace super_resolution {
 
 ImageData MapSolver::Solve() const {
-  // TODO: implement.
-  ImageData data;
-  return data;
+  // TODO: get an initial estimate.
+  ImageData estimated_image;
+
+  /* TODO: this is just "example" code in Ceres, if that is the solver to use.
+   * This needs to be figured out and actually implemented.
+
+  // TODO: how many channels?
+  const int num_channels = 0;
+
+  ceres::Problem problem;
+  // TODO: currently solves independently for each channel.
+  for (int channel = 0; channel < num_channels; ++channel) {
+    for (const ImageData& observation : low_res_images_) {
+      ceres::CostFunction* cost_function = MapCostFunction::Create(
+          observation.GetPixelDataForChannel(channel), image_model_, channel);
+      problem.AddResidualBlock(
+          cost_function,
+          loss_function,
+          estimated_image.GetPixelDataForChannel(channel));
+    }
+  }
+
+  // TODO: handle regularization.
+
+  // Set the solver options. TODO: figure out what these should be.
+  ceres::Solver::Options options;
+  options.linear_solver_type = ceres::DENSE_SCHUR;
+  options.minimizer_progress_to_stdout = true;
+
+  // Solve.
+  ceres::Solver::Summary summary;
+  ceres::Solve(options, &problem, &summary);
+  std::cout << summary.FullReport() << std::endl;
+
+  */
+
+  return estimated_image;
 }
 
 }  // namespace super_resolution
