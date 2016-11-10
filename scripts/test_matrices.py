f979876b12fd1ddf5df523ac9f2c1cc2cb6451df
Richard Teammco <teammcr192@gmail.com>
2016-11-10 13:15:58 -0600

    Added conv kernel => matrix*vector prototype code.
    This converts an arbitrary convolution kernel into a matrix. Multiply this
    kernel matrix with the vectorized image to get the same output as the
    convolution.

--- scripts/test_matrices.py
+++ scripts/test_matrices.py
@@ -28,13 +28,55 @@ def downsample(image, scale):
   DI = DIv.reshape(nrows / scale, ncols / scale)
   print 'Final downsampled image:'
   print DI
+  return DI
+
+def conv_kernel(image, kernel):
+  print 'Kernel:'
+  print kernel
+  # Image dimensions:
+  nrows = image.shape[0]
+  ncols = image.shape[1]
+  # Kernel dimensions:
+  krows = kernel.shape[0]
+  kcols = kernel.shape[1]
+  # Figure out relative kernel offsets:
+  mid_row = krows / 2
+  mid_col = kcols / 2
+  offsets = []
+  for row in range(krows):
+    for col in range(kcols):
+      offsets.append((row - mid_row, col - mid_col))
+  # Build the operator matrix:
+  K = []
+  for row in range(nrows):
+    for col in range(ncols):
+      K.append([0] * (nrows * ncols))
+      for offset in offsets:
+        image_row = row + offset[0]
+        image_col = col + offset[1]
+        if (0 <= image_row < nrows) and (0 <= image_col < ncols):
+          index = image_row * ncols + image_col
+          kernel_coords = (offset[0] + mid_row, offset[1] + mid_col)
+          K[-1][index] = kernel[kernel_coords]
+  print 'Kernel matrix:'
+  print K
+  print 'Convolved image:'
+  Iv = image.reshape(nrows * ncols, 1)  # vectorize the image
+  KIv = K * Iv
+  KI = KIv.reshape(nrows, ncols)  # un-vectorize the image back
+  print KI
 
 if __name__ == '__main__':
-  I  = np.matrix(
+  I = np.matrix(
       [[1, 2, 3, 4, 5, 6],
        [7, 8, 9, 0, 1, 2],
        [9, 7, 5, 4, 2, 1],
        [2, 4, 6, 8, 0, 1]])
   print 'Test image:'
   print I
-  downsample(I, 2)
+  I = downsample(I, 2)
+  kernel = np.matrix(
+      [[-1, 0, 1],
+       [-2, 0, 2],
+       [-1, 0, 1]])
+  conv_kernel(I, kernel)
