62da3ebd3dc34738c291877773c3e02f02f2e985
Richard Teammco <teammcr192@gmail.com>
2016-11-11 11:45:36 -0600

    Implemented downsampling matrix getter.
    Test passed.

--- src/image_model/downsampling_module.cpp
+++ src/image_model/downsampling_module.cpp
@@ -9,14 +9,14 @@
 
 namespace super_resolution {
 
-DownsamplingModule::DownsamplingModule(const double scale) : scale_(scale) {
-  CHECK_GE(scale_, 1.0);
+DownsamplingModule::DownsamplingModule(const int scale) : scale_(scale) {
+  CHECK_GE(scale_, 1);
 }
 
 void DownsamplingModule::ApplyToImage(
     ImageData* image_data, const int index) const {
 
-  const double scale_factor = 1.0 / scale_;
+  const double scale_factor = 1.0 / static_cast<double>(scale_);
   // Area interpolation method aliases images by dropping pixels.
   image_data->ResizeImage(scale_factor, cv::INTER_AREA);
 }
@@ -24,9 +24,25 @@ void DownsamplingModule::ApplyToImage(
 cv::Mat DownsamplingModule::GetOperatorMatrix(
     const cv::Size& image_size, const int index) const {
 
-  // TODO: implement.
-  const int num_pixels = image_size.width * image_size.height;
-  return cv::Mat::eye(num_pixels, num_pixels, CV_64FC1);
+  const int num_high_res_pixels = image_size.width * image_size.height;
+  const int num_low_res_pixels = num_high_res_pixels / (scale_ * scale_);
+  cv::Mat downsampling_matrix =
+      cv::Mat::zeros(num_low_res_pixels, num_high_res_pixels, CV_64FC1);
+  int next_row = 0;
+  for (int row = 0; row < image_size.height; ++row) {
+    if (row % scale_ != 0) {
+      continue;
+    }
+    for (int col = 0; col < image_size.width; ++col) {
+      if (col % scale_ != 0) {
+        continue;
+      }
+      const int index = row * image_size.width + col;
+      downsampling_matrix.at<double>(next_row, index) = 1;
+      next_row++;
+    }
+  }
+  return downsampling_matrix;
 }
 
 }  // namespace super_resolution
--- src/image_model/downsampling_module.h
+++ src/image_model/downsampling_module.h
@@ -15,7 +15,7 @@ class DownsamplingModule : public DegradationOperator {
  public:
   // The given scale parameter is the scale by which the resized image will
   // be modified. The scale should be greater than or equal to 1.
-  explicit DownsamplingModule(const double scale);
+  explicit DownsamplingModule(const int scale);
 
   virtual void ApplyToImage(ImageData* image_data, const int index) const;
 
@@ -23,7 +23,7 @@ class DownsamplingModule : public DegradationOperator {
       const cv::Size& image_size, const int index) const;
 
  private:
-  const double scale_;
+  const int scale_;
 };
 
 }  // namespace super_resolution
