324cc9849cb628ff106d2c08f144ac5bd4dfeaf2
Richard Teammco <teammcr192@gmail.com>
2017-01-22 13:31:07 -0600

    Started fixing TV Regularizer derivatives test.

--- test/test_tv_regularizer.cpp
+++ test/test_tv_regularizer.cpp
@@ -16,42 +16,47 @@ using testing::SizeIs;
 // Minimum divisor so we prevent division by zero.
 constexpr double kMinDivisor = 0.000001;
 
+// Small test image and expected returned values for this data.
+const cv::Size test_image_size(3, 3);
+const double test_image_data[9] = {
+   0,  0, 1,
+   0,  1, 3,
+  -3, -1, 0
+};
+// The x-direction gradients (not squared) should be:
+//
+//  | 0 | 1 | 0 |                | 0 | 1 | 0 |
+//  | 1 | 2 | 0 |     squared=>  | 1 | 4 | 0 |
+//  | 2 | 1 | 0 |                | 4 | 1 | 0 |
+//
+// and the y-direction gradients should be:
+//
+//  |  0 |  1 |  2 |             | 0 | 1 | 4 |
+//  | -3 | -2 | -3 |  squared=>  | 9 | 4 | 9 |
+//  |  0 |  0 |  0 |             | 0 | 0 | 0 |
+//
+// So at each pixel, the square roots of the squared sums are expected to be
+// sqrt(y*y + x*x):
+const std::vector<double> test_image_expected_residuals = {
+  sqrt(0 + 0), sqrt(1 + 1), sqrt(0 + 4),
+  sqrt(1 + 9), sqrt(4 + 4), sqrt(0 + 9),
+  sqrt(4 + 0), sqrt(1 + 0), sqrt(0 + 0)
+};
+
 // Verifies that the TV regularizer correctly computes the expected residuals
 // for an image.
 TEST(TotalVariationRegularizer, ApplyToImage) {
-  const cv::Size image_size(3, 3);
-  const super_resolution::TotalVariationRegularizer tv_regularizer(image_size);
-
-  const double image1_data[9] = {
-     0,  0, 1,
-     0,  1, 3,
-    -3, -1, 0
-  };
-  // The x-direction gradients (not squared) should be:
-  //
-  //  | 0 | 1 | 0 |                | 0 | 1 | 0 |
-  //  | 1 | 2 | 0 |     squared=>  | 1 | 4 | 0 |
-  //  | 2 | 1 | 0 |                | 4 | 1 | 0 |
-  //
-  // and the y-direction gradients should be:
-  //
-  //  |  0 |  1 |  2 |             | 0 | 1 | 4 |
-  //  | -3 | -2 | -3 |  squared=>  | 9 | 4 | 9 |
-  //  |  0 |  0 |  0 |             | 0 | 0 | 0 |
-  //
-  // So at each pixel, the square roots of the squared sums are expected to be
-  // sqrt(y*y + x*x):
-  const std::vector<double> expected_residuals = {
-    sqrt(0 + 0), sqrt(1 + 1), sqrt(0 + 4),
-    sqrt(1 + 9), sqrt(4 + 4), sqrt(0 + 9),
-    sqrt(4 + 0), sqrt(1 + 0), sqrt(0 + 0)
-  };
-
+  const super_resolution::TotalVariationRegularizer tv_regularizer(
+      test_image_size);
   const std::vector<double> returned_residuals =
-      tv_regularizer.ApplyToImage(image1_data);
-  EXPECT_THAT(returned_residuals, ContainerEq(expected_residuals));
+      tv_regularizer.ApplyToImage(test_image_data);
+  EXPECT_THAT(returned_residuals, ContainerEq(test_image_expected_residuals));
+}
 
-  /* Now verify that the derivatives are being computed correctly. */
+// Verifies that the derivatives are also being computed correctly.
+TEST(TotalVariationRegularizer, ApplyToImageWithDifferentiation) {
+  const super_resolution::TotalVariationRegularizer tv_regularizer(
+      test_image_size);
 
   // Given the following indices of the image:
   //
@@ -59,76 +64,64 @@ TEST(TotalVariationRegularizer, ApplyToImage) {
   //  | 3 | 4 | 5 |
   //  | 6 | 7 | 8 |
   //
-  // we should expect the following relevant partial derivatives (each pixel
-  // w.r.t. itself, the pixel to the left, and the pixel above it):
+  // we should expect the following relevant partial derivatives - for each
+  // pixel, all other pixels w.r.t. that pixel (itself, the one below, and the
+  // one to the right).
   //
   //        self    left    above
-  // d/d0 = d0/d0 +   0   +   0     <= no pixels to the left or above
-  // d/d1 = d1/d1 + d1/d0 +   0     <= no pixel above
-  // d/d2 = d2/d2 + d2/d1 +   0     <= no pixel above
-  // d/d3 = d3/d3 +   0   + d3/d0   <= no pixel to the left
-  // d/d4 = d4/d4 + d4/d3 + d4/d1
-  // d/d5 = d5/d5 + d5/d4 + d5/d2
-  // d/d6 = d6/d6 +   0   + d6/d3   <= no pixel to the left
-  // d/d7 = d7/d7 + d7/d6 + d7/d4
-  // d/d8 = d8/d8 + d8/d7 + d8/d5
+  // d/d0 = d0/d0 + d1/d0 + d3/d0
+  // d/d1 = d1/d1 + d2/d1 + d4/d1
+  // d/d2 = d2/d2 +   0   + d5/d2   <= no pixel to the right
+  // d/d3 = d3/d3 + d4/d3 + d6/d3
+  // d/d4 = d4/d4 + d5/d4 + d7/d4
+  // d/d5 = d5/d5 +   0   + d8/d5   <= no pixel to the right
+  // d/d6 = d6/d6 + d7/d6 +   0     <= no pixel below
+  // d/d7 = d7/d7 + d8/d7 +   0     <= no pixel below
+  // d/d8 = d8/d8 +   0   +   0     <= no pixel to the right or below
   //
   // The partials should be (where the numbers represent the pixel values at
   // that image index):
-  //   d0/d0 = ((1 - 0) + (3 - 0)) / tv(0)
-  //   d1/d1 = ((2 - 1) + (4 - 1)) / tv(1)
-  //   d1/d0 = -(1 - 0) / tv(0)
-  //   d2/d2 = (5 - 2) / tv(2)
-  //   d2/d1 = -(2 - 1) / tv(1)
-  //   d3/d3 = ((4 - 3) + (6 - 3)) / tv(3)
-  //   d3/d0 = -(3 - 0) / tv(0)
-  //   d4/d4 = ((4 - 3) + (7 - 4)) / tv(4)
-  //   d4/d3 = -(4 - 3) / tv(3)
-  //   d4/d1 = -(4 - 1) / tv(1)
-  //   d8/d8 = zero (edge case)
-  //   d8/d7 = -(8 - 7) / tv(7)
-  //   d8/d5 = -(8 - 5) / tv(5)
+  // TODO: fix this.
   // The others follow similarly, but we won't test those.
 
   // First, compute the "total variation", which is just the expected residuals
   // but padding zero values with a small delta to avoid division by 0. Total
   // variation is always non-negative.
   std::vector<double> total_variation;
-  for (const double residual : expected_residuals) {
+  for (const double residual : test_image_expected_residuals) {
     total_variation.push_back(std::max(residual, kMinDivisor));
   }
 
-  // The actual derivatives, calculated from the test image, thus are:
-  // TODO: this is WRONG and needs to be updated. Test fails.
+  // The partial derivatives, calculated from the test image, thus are:
   const double d0d0 =
-      ((image1_data[1] - image1_data[0]) +
-      (image1_data[3] - image1_data[0])) / total_variation[0];
+      ((test_image_data[1] - test_image_data[0]) +
+      (test_image_data[3] - test_image_data[0])) / total_variation[0];
   const double d1d1 =
-      ((image1_data[2] - image1_data[1]) +
-      (image1_data[4] - image1_data[1])) / total_variation[1];
+      ((test_image_data[2] - test_image_data[1]) +
+      (test_image_data[4] - test_image_data[1])) / total_variation[1];
   const double d1d0 =
-      -(image1_data[1] - image1_data[0]) / total_variation[0];
+      -(test_image_data[1] - test_image_data[0]) / total_variation[0];
   const double d2d2 =
-      (image1_data[5] - image1_data[2]) / total_variation[2];
+      (test_image_data[5] - test_image_data[2]) / total_variation[2];
   const double d2d1 =
-      -(image1_data[2] - image1_data[1]) / total_variation[1];
+      -(test_image_data[2] - test_image_data[1]) / total_variation[1];
   const double d3d3 =
-      ((image1_data[4] - image1_data[3]) +
-      (image1_data[6] - image1_data[3])) / total_variation[3];
+      ((test_image_data[4] - test_image_data[3]) +
+      (test_image_data[6] - test_image_data[3])) / total_variation[3];
   const double d3d0 =
-      -(image1_data[3] - image1_data[0]) / total_variation[0];
+      -(test_image_data[3] - test_image_data[0]) / total_variation[0];
   const double d4d4 =
-      ((image1_data[5] - image1_data[4]) +
-      (image1_data[7] - image1_data[4])) / total_variation[4];
+      ((test_image_data[5] - test_image_data[4]) +
+      (test_image_data[7] - test_image_data[4])) / total_variation[4];
   const double d4d3 =
-      -(image1_data[4] - image1_data[3]) / total_variation[3];
+      -(test_image_data[4] - test_image_data[3]) / total_variation[3];
   const double d4d1 =
-      -(image1_data[4] - image1_data[1]) / total_variation[1];
+      -(test_image_data[4] - test_image_data[1]) / total_variation[1];
   const double d8d8 = 0;  // Edge case, no TV defined in corner....
   const double d8d7 =
-      -(image1_data[8] - image1_data[7]) / total_variation[7];
+      -(test_image_data[8] - test_image_data[7]) / total_variation[7];
   const double d8d5 =
-      -(image1_data[8] - image1_data[5]) / total_variation[5];
+      -(test_image_data[8] - test_image_data[5]) / total_variation[5];
 
   // The expected values for the first four derivatives.
   const double expected_d0 = d0d0;
@@ -145,7 +138,7 @@ TEST(TotalVariationRegularizer, ApplyToImage) {
 
   const auto& returned_residuals_and_gradient_automatic =
       tv_regularizer.ApplyToImageWithDifferentiation(
-          image1_data,
+          test_image_data,
           gradient_constants,
           super_resolution::AUTOMATIC_DIFFERENTIATION);
   const std::vector<double> gradient_automatic =
@@ -154,7 +147,7 @@ TEST(TotalVariationRegularizer, ApplyToImage) {
   // Check correct behavior of returned residuals.
   EXPECT_THAT(
       returned_residuals_and_gradient_automatic.first,
-      ContainerEq(expected_residuals));
+      ContainerEq(test_image_expected_residuals));
 
   EXPECT_THAT(gradient_automatic, SizeIs(9));
 
@@ -169,7 +162,7 @@ TEST(TotalVariationRegularizer, ApplyToImage) {
 
   const auto& returned_residuals_and_gradient_analytical =
       tv_regularizer.ApplyToImageWithDifferentiation(
-          image1_data,
+          test_image_data,
           gradient_constants,
           super_resolution::ANALYTICAL_DIFFERENTIATION);
   const std::vector<double> gradient_analytical =
@@ -178,7 +171,7 @@ TEST(TotalVariationRegularizer, ApplyToImage) {
   // Check correct behavior of returned residuals.
   EXPECT_THAT(
       returned_residuals_and_gradient_analytical.first,
-      ContainerEq(expected_residuals));
+      ContainerEq(test_image_expected_residuals));
 
   EXPECT_THAT(gradient_analytical, SizeIs(9));
 
