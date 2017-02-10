dd388570000c82669204e0ac96806cbfd02b7f4c
Richard Teammco <teammcr192@gmail.com>
2017-02-10 12:09:00 -0600

    Updated the way some std::vectors get allocated.
    Before they initialized all values to 0, which was a bit time-consuming. Now the vectors that do not need to be initialized with default values are not pre-allocated. This should save a small amount of allocation time.

--- src/optimization/irls_map_solver.cpp
+++ src/optimization/irls_map_solver.cpp
@@ -113,7 +113,8 @@ std::pair<double, std::vector<double>> IrlsMapSolver::ComputeDataTerm(
   // for the final residual sum.
   double residual_sum = 0;
   const int num_data_points = GetNumDataPoints();
-  std::vector<double> residuals(num_data_points);
+  std::vector<double> residuals;
+  residuals.reserve(num_data_points);
   const int num_pixels = GetNumPixels();
   for (int channel_index = 0; channel_index < num_channels; ++channel_index) {
     const double* degraded_hr_channel_data =
@@ -124,8 +125,7 @@ std::pair<double, std::vector<double>> IrlsMapSolver::ComputeDataTerm(
       const double residual =
           degraded_hr_channel_data[pixel_index] -
           observation_channel_data[pixel_index];
-      const int data_index = channel_index * num_pixels + pixel_index;
-      residuals[data_index] = residual;
+      residuals.push_back(residual);
       residual_sum += (residual * residual);
     }
   }
@@ -140,14 +140,13 @@ std::pair<double, std::vector<double>> IrlsMapSolver::ComputeDataTerm(
   image_model_.ApplyTransposeToImage(&residual_image, image_index);
 
   // Build the gradient vector.
-  std::vector<double> gradient(num_data_points);
+  std::vector<double> gradient;
+  gradient.reserve(num_data_points);
   for (int channel_index = 0; channel_index < num_channels; ++channel_index) {
     const double* residual_channel_data =
         residual_image.GetChannelData(channel_index);
     for (int pixel_index = 0; pixel_index < num_pixels; ++pixel_index) {
-      const double gradient_at_pixel = 2 * residual_channel_data[pixel_index];
-      const int data_index = channel_index * num_pixels + pixel_index;
-      gradient[data_index] = gradient_at_pixel;
+      gradient.push_back(2 * residual_channel_data[pixel_index]);
     }
   }
 
@@ -169,13 +168,16 @@ std::pair<double, std::vector<double>> IrlsMapSolver::ComputeRegularization(
 
     // Precompute the constant terms in the gradients at each pixel. This is
     // the regularization parameter (lambda) and the IRLS weights.
-    std::vector<double> gradient_constants(num_data_points);
+    std::vector<double> gradient_constants;
+    gradient_constants.reserve(num_data_points);
     for (int i = 0; i < num_data_points; ++i) {
-      gradient_constants[i] = regularization_parameter * irls_weights_.at(i);
+      gradient_constants.push_back(
+          regularization_parameter * irls_weights_.at(i));
     }
 
     // Compute the residuals and squared residual sum.
-    std::pair<std::vector<double>, std::vector<double>> values_and_partials =
+    const std::pair<std::vector<double>, std::vector<double>>&
+    values_and_partials =
         regularizer_and_parameter.first->ApplyToImageWithDifferentiation(
             estimated_image_data, gradient_constants);
 
@@ -205,7 +207,8 @@ void IrlsMapSolver::UpdateIrlsWeights(const double* estimated_image_data) {
   // (which means we need separate weights for each one).
   const int num_data_points = GetNumDataPoints();
   for (const auto& regularizer_and_parameter : regularizers_) {
-    std::vector<double> regularization_residuals =
+    // TODO: maybe keep this from the computation phase to avoid recomputing?
+    const std::vector<double>& regularization_residuals =
         regularizer_and_parameter.first->ApplyToImage(estimated_image_data);
     CHECK_EQ(regularization_residuals.size(), num_data_points)
         << "Number of residuals does not match number of weights.";
