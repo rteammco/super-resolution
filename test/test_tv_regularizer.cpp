716e04b29ca507c5a181370ed4408ddb565cffd1
Richard Teammco <teammcr192@gmail.com>
2017-02-10 14:48:59 -0600

    Updated TV Regularizer test w/ gradient test.

--- test/test_tv_regularizer.cpp
+++ test/test_tv_regularizer.cpp
@@ -42,12 +42,6 @@ const std::vector<double> test_image_expected_residuals_1_norm = {
   (1 + 3), (2 + 2), (0 + 3),
   (2 + 0), (1 + 0), (0 + 0)
 };
-// TODO: probably remove 2-norm tests.
-const std::vector<double> test_image_expected_residuals_2_norm = {
-  sqrt(0 + 0), sqrt(1 + 1), sqrt(0 + 4),
-  sqrt(1 + 9), sqrt(4 + 4), sqrt(0 + 9),
-  sqrt(4 + 0), sqrt(1 + 0), sqrt(0 + 0)
-};
 
 // Replicates a vector the given number of times to extend it. The new elements
 // will be exact copies of the original element sequence.
@@ -155,104 +149,50 @@ TEST(TotalVariationRegularizer, ApplyToImage3d) {
 // Verifies that the derivatives are also being computed correctly.
 TEST(TotalVariationRegularizer, ApplyToImageWithDifferentiation) {
   const super_resolution::TotalVariationRegularizer tv_regularizer(
-      test_image_size, 1);  // TODO: testing only 1 channel. Test multiple.
+      test_image_size, 1);
 
-  // Given the following indices of the image:
-  //
-  //  | 0 | 1 | 2 |
-  //  | 3 | 4 | 5 |
-  //  | 6 | 7 | 8 |
-  //
-  // we should expect the following relevant partial derivatives - for each
-  // pixel, all other pixels w.r.t. that pixel (itself, the one below, and the
-  // one to the right).
-  //
-  //        self    left    above
-  // d/d0 = d0/d0 + d1/d0 + d3/d0
-  // d/d1 = d1/d1 + d2/d1 + d4/d1
-  // d/d2 = d2/d2 +   0   + d5/d2   <= no pixel to the right
-  // d/d3 = d3/d3 + d4/d3 + d6/d3
-  // d/d4 = d4/d4 + d5/d4 + d7/d4
-  // d/d5 = d5/d5 +   0   + d8/d5   <= no pixel to the right
-  // d/d6 = d6/d6 + d7/d6 +   0     <= no pixel below
-  // d/d7 = d7/d7 + d8/d7 +   0     <= no pixel below
-  // d/d8 = d8/d8 +   0   +   0     <= no pixel to the right or below
-  //
-  // The partials should be (where the numbers represent the pixel values at
-  // that image index):
-  // TODO: fix this.
-  // The others follow similarly, but we won't test those.
-
-  // First, compute the "total variation", which is just the expected residuals
-  // but padding zero values with a small delta to avoid division by 0. Total
-  // variation is always non-negative.
-  std::vector<double> total_variation;
-  for (const double residual : test_image_expected_residuals_2_norm) {
-    total_variation.push_back(std::max(residual, kMinDivisor));
-  }
-
-  // The partial derivatives, calculated from the test image, thus are:
-  const double d0d0 =
-      ((test_image_data[1] - test_image_data[0]) +
-      (test_image_data[3] - test_image_data[0])) / total_variation[0];
-  const double d1d1 =
-      ((test_image_data[2] - test_image_data[1]) +
-      (test_image_data[4] - test_image_data[1])) / total_variation[1];
-  const double d1d0 =
-      -(test_image_data[1] - test_image_data[0]) / total_variation[0];
-  const double d2d2 =
-      (test_image_data[5] - test_image_data[2]) / total_variation[2];
-  const double d2d1 =
-      -(test_image_data[2] - test_image_data[1]) / total_variation[1];
-  const double d3d3 =
-      ((test_image_data[4] - test_image_data[3]) +
-      (test_image_data[6] - test_image_data[3])) / total_variation[3];
-  const double d3d0 =
-      -(test_image_data[3] - test_image_data[0]) / total_variation[0];
-  const double d4d4 =
-      ((test_image_data[5] - test_image_data[4]) +
-      (test_image_data[7] - test_image_data[4])) / total_variation[4];
-  const double d4d3 =
-      -(test_image_data[4] - test_image_data[3]) / total_variation[3];
-  const double d4d1 =
-      -(test_image_data[4] - test_image_data[1]) / total_variation[1];
-  const double d8d8 = 0;  // Edge case, no TV defined in corner....
-  const double d8d7 =
-      -(test_image_data[8] - test_image_data[7]) / total_variation[7];
-  const double d8d5 =
-      -(test_image_data[8] - test_image_data[5]) / total_variation[5];
-
-  // The expected values for the first four derivatives.
-  const double expected_d0 = d0d0;
-  const double expected_d1 = d1d1 + d1d0;
-  const double expected_d2 = d2d2 + d2d1;
-  const double expected_d3 = d3d3 + d3d0;
-  const double expected_d4 = d4d4 + d4d3 + d4d1;
-  const double expected_d8 = d8d8 + d8d7 + d8d5;
-
-  // Pass identity (1) as weights, so we should expect the pure partial
-  // derivatives be returned.
+  // Keep all constants neutral for this test.
   std::vector<double> gradient_constants(9);
   std::fill(gradient_constants.begin(), gradient_constants.end(), 1.0);
 
-  const auto& returned_residuals_and_gradient =
+  const auto& residuals_and_gradient =
       tv_regularizer.ApplyToImageWithDifferentiation(
-          test_image_data.data(),
-          gradient_constants);
-  const std::vector<double> gradient =
-      returned_residuals_and_gradient.second;
+          test_image_data.data(), gradient_constants);
+  const std::vector<double>& residuals = residuals_and_gradient.first;
+  const std::vector<double>& gradient = residuals_and_gradient.second;
+  EXPECT_THAT(residuals, ContainerEq(test_image_expected_residuals_1_norm));
 
-  // Check correct behavior of returned residuals.
-  EXPECT_THAT(
-      returned_residuals_and_gradient.first,
-      ContainerEq(test_image_expected_residuals_2_norm));
+  // Compute the true approximate gradient using finite differences (numerical
+  // differentiation).
+  const double finite_difference = 1e-6;
+  const double gradient_error_tolerance = 0.0001;
+  for (int i = 0; i < 9; ++i) {
+    // Get residual with positive finite difference (+d).
+    std::vector<double> pos_diff_image_data = test_image_data;
+    pos_diff_image_data[i] += finite_difference;
+    const std::vector<double> pos_diff_residuals =
+        tv_regularizer.ApplyToImage(pos_diff_image_data.data());
+    double pos_diff_residual = 0.0;
+    for (const double residual : pos_diff_residuals) {
+      // The residuals are squared because the analytical differentiation is
+      // implemented assuming the 2-norm.
+      pos_diff_residual += (residual * residual);
+    }
 
-  EXPECT_THAT(gradient, SizeIs(9));
+    // Get residual with negative finite difference (-d).
+    std::vector<double> neg_diff_image_data = test_image_data;
+    neg_diff_image_data[i] -= finite_difference;
+    const std::vector<double> neg_diff_residuals =
+        tv_regularizer.ApplyToImage(neg_diff_image_data.data());
+    double neg_diff_residual = 0.0;
+    for (const double residual : neg_diff_residuals) {
+      neg_diff_residual += (residual * residual);
+    }
 
-  EXPECT_EQ(gradient[0], expected_d0);
-  EXPECT_EQ(gradient[1], expected_d1);
-  EXPECT_EQ(gradient[2], expected_d2);
-  EXPECT_EQ(gradient[3], expected_d3);
-  EXPECT_EQ(gradient[4], expected_d4);
-  EXPECT_EQ(gradient[8], expected_d8);
+    // Compute the partial w.r.t. variable i:
+    const double numerical_gradient_at_i =
+        (pos_diff_residual - neg_diff_residual) /
+        (2 * finite_difference);
+    EXPECT_NEAR(numerical_gradient_at_i, gradient[i], gradient_error_tolerance);
+  }
 }
