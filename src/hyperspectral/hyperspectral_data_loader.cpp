8593281be4438dc8c3769d08949f6495acf59901
Richard Teammco <teammcr192@gmail.com>
2017-03-07 15:58:57 -0600

    Implemented BSQ reading. Needs to be tested!

--- src/hyperspectral/hyperspectral_data_loader.cpp
+++ src/hyperspectral/hyperspectral_data_loader.cpp
@@ -20,18 +20,110 @@ namespace hyperspectral {
 
 constexpr char kMatlabTextDataDelimiter = ',';
 
-ImageData ReadBinaryFile(
-    const std::string& file_path,
-    const HSIBinaryDataParameters& parameters,
-    const int start_row,
-    const int end_row,
-    const int start_col,
-    const int end_col,
-    const int start_band,
-    const int end_band) {
+struct HSIDataRange {
+  int start_row = 0;
+  int start_col = 0;
+  int end_row = 0;
+  int end_col = 0;
+  int start_band = 0;
+  int end_band = 0;
+};
 
-  // TODO: implement.
-  return ImageData();
+// Reverses the bytes of the given value (e.g. float). This is used to convert
+// from the data's endian form into the machine's endian form when they are not
+// matched up.
+template <typename T>
+T ReverseBytes(const T value) {
+  T reversed_value;
+  const unsigned char* original_bytes = (const unsigned char*)(&value);
+  unsigned char* reversed_bytes  = (unsigned char*)(&reversed_value);
+  const int num_bytes = sizeof(T);
+  for (int i = 0; i < num_bytes; ++i) {
+    reversed_bytes[i] = original_bytes[num_bytes - 1 - i];
+  }
+  return reversed_value;
+}
+
+template <typename T>
+ImageData ReadBinaryFileBSQ(
+    const std::string& hsi_file_path,
+    const int num_data_rows,
+    const int num_data_cols,
+    const int num_data_bands,
+    const long start_index,
+    const bool reverse_bytes,
+    const HSIDataRange& data_range) {
+
+  std::ifstream input_file(hsi_file_path);
+  CHECK(input_file.is_open())
+      << "File '" << hsi_file_path << "' could not be opened for reading.";
+
+  const int data_point_size = sizeof(T);
+  long current_index = start_index;
+  input_file.seekg(current_index * data_point_size);
+
+  ImageData hsi_image;
+  const cv::Size image_size(num_data_cols, num_data_rows);
+  const long num_pixels = num_data_rows * num_data_cols;
+  for (int band = data_range.start_band; band < data_range.end_band; ++band) {
+    const long band_index = band * num_pixels;
+    cv::Mat channel_image(
+        num_data_cols, num_data_rows, util::kOpenCvMatrixType);
+    for (int row = data_range.start_row; row < data_range.end_row; ++row) {
+      const int channel_row = row - data_range.start_row;
+      for (int col = data_range.start_col; col < data_range.end_col; ++col) {
+        const int channel_col = col - data_range.start_col;
+        const long pixel_index = row * num_data_cols + col;
+        const long next_index = band_index + pixel_index;
+        // Skip to next position if necessary.
+        if (next_index > (current_index + 1)) {
+          input_file.seekg(next_index * data_point_size);
+        }
+        T value;
+        input_file.read(reinterpret_cast<char*>(&value), data_point_size);
+        if (reverse_bytes) {
+          value = ReverseBytes<T>(value);
+        }
+        channel_image.at<double>(channel_row, channel_col) =
+            static_cast<double>(value);
+        current_index = next_index;
+      }
+    }
+    hsi_image.AddChannel(channel_image);
+  }
+  input_file.close();
+  return hsi_image;
+}
+
+ImageData ReadBinaryFile(
+    const std::string& hsi_file_path,
+    const HSIBinaryDataParameters& parameters,
+    const HSIDataRange& data_range) {
+
+  // Determine the machine endian. Union of memory means "unsigned int value"
+  // and "unsigned char bytes" share the same memory.
+  union UnsignedNumber {
+    unsigned int value;
+    unsigned char bytes[sizeof(unsigned int)];
+  };
+  // Set the value to unsigned 1, and check the byte array to see if it is in
+  // big endian or little endian order. The left-most byte will be empty (zero)
+  // if the machine is big endian.
+  UnsignedNumber number;
+  number.value = 1U;  // Unsigned int 1.
+  const bool machine_big_endian = (number.bytes[0] != 1U);
+  // If endians don't match, the bytes from the file have to be reversed.
+  const bool reverse_bytes = (parameters.big_endian != machine_big_endian);
+
+  // TODO: This may change, depending on interleave format and data type.
+  return ReadBinaryFileBSQ<float>(
+      hsi_file_path,
+      parameters.num_data_rows,
+      parameters.num_data_cols,
+      parameters.num_data_bands,
+      parameters.header_offset,
+      reverse_bytes,
+      data_range);
 }
 
 void HSIBinaryDataParameters::ReadHeaderFromFile(
@@ -204,62 +296,58 @@ void HyperspectralDataLoader::LoadDataFromBinaryFile() {
       << "Number of data bands must be positive.";
 
   // Now get the data range parameters.
+  HSIDataRange data_range;
   // Start row:
   const std::string start_row_string =
       util::GetConfigValueOrDie(config_file_map, "start_row");
-  const int start_row = std::atoi(start_row_string.c_str());
-  CHECK_GE(start_row, 0) << "Start row index cannot be negative.";
-  CHECK_LT(start_row, parameters.num_data_rows)
+  data_range.start_row = std::atoi(start_row_string.c_str());
+  CHECK_GE(data_range.start_row, 0) << "Start row index cannot be negative.";
+  CHECK_LT(data_range.start_row, parameters.num_data_rows)
       << "Start row index is out of bounds.";
   // End row:
   const std::string end_row_string =
       util::GetConfigValueOrDie(config_file_map, "end_row");
-  const int end_row = std::atoi(end_row_string.c_str());
-  CHECK_GT(end_row, 0) << "End row index must be positive.";
-  CHECK_LE(end_row, parameters.num_data_rows)
+  data_range.end_row = std::atoi(end_row_string.c_str());
+  CHECK_GT(data_range.end_row, 0) << "End row index must be positive.";
+  CHECK_LE(data_range.end_row, parameters.num_data_rows)
       << "End row index is out of bounds.";
-  CHECK_GT(end_row - start_row, 0) << "Row range must be positive.";
+  CHECK_GT(data_range.end_row - data_range.start_row, 0)
+      << "Row range must be positive.";
   // Start column:
   const std::string start_col_string =
       util::GetConfigValueOrDie(config_file_map, "start_col");
-  const int start_col = std::atoi(start_col_string.c_str());
-  CHECK_GE(start_col, 0) << "Start column index cannot be negative.";
-  CHECK_LT(start_col, parameters.num_data_cols)
+  data_range.start_col = std::atoi(start_col_string.c_str());
+  CHECK_GE(data_range.start_col, 0) << "Start column index cannot be negative.";
+  CHECK_LT(data_range.start_col, parameters.num_data_cols)
       << "Start column index is out of bounds.";
   // End column:
   const std::string end_col_string =
       util::GetConfigValueOrDie(config_file_map, "end_col");
-  const int end_col = std::atoi(end_col_string.c_str());
-  CHECK_GT(end_col, 0) << "End column index must be positive.";
-  CHECK_LE(end_col, parameters.num_data_cols)
+  data_range.end_col = std::atoi(end_col_string.c_str());
+  CHECK_GT(data_range.end_col, 0) << "End column index must be positive.";
+  CHECK_LE(data_range.end_col, parameters.num_data_cols)
       << "End column index is out of bounds.";
-  CHECK_GT(end_col - start_col, 0) << "Column range must be positive.";
+  CHECK_GT(data_range.end_col - data_range.start_col, 0)
+      << "Column range must be positive.";
   // Start band:
   const std::string start_band_string =
       util::GetConfigValueOrDie(config_file_map, "start_band");
-  const int start_band = std::atoi(start_band_string.c_str());
-  CHECK_GE(start_band, 0) << "Start band index cannot be negative.";
-  CHECK_LT(start_band, parameters.num_data_bands)
+  data_range.start_band = std::atoi(start_band_string.c_str());
+  CHECK_GE(data_range.start_band, 0) << "Start band index cannot be negative.";
+  CHECK_LT(data_range.start_band, parameters.num_data_bands)
       << "Start band index is out of bounds.";
   // End band:
   const std::string end_band_string =
       util::GetConfigValueOrDie(config_file_map, "end_band");
-  const int end_band = std::atoi(end_band_string.c_str());
-  CHECK_GT(end_band, 0) << "End band index must be positive.";
-  CHECK_LE(end_band, parameters.num_data_bands)
+  data_range.end_band = std::atoi(end_band_string.c_str());
+  CHECK_GT(data_range.end_band, 0) << "End band index must be positive.";
+  CHECK_LE(data_range.end_band, parameters.num_data_bands)
       << "End band index is out of bounds.";
-  CHECK_GT(end_band - start_band, 0) << "Band range must be positive.";
+  CHECK_GT(data_range.end_band - data_range.start_band, 0)
+      << "Band range must be positive.";
 
   // And finally, read the data according to the parameters and range.
-  ReadBinaryFile(
-      hsi_file_path,
-      parameters,
-      start_row,
-      end_row,
-      start_col,
-      end_col,
-      start_band,
-      end_band);
+  ReadBinaryFile(hsi_file_path, parameters, data_range);
 }
 
 ImageData HyperspectralDataLoader::GetImage() const {
