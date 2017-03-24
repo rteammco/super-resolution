dd1cc4ac07ff690d4209990e6f2539af6f583162
Richard Teammco <teammcr192@gmail.com>
2017-03-24 14:38:58 -0500

    Translational registration now works.
    TODO: The code needs serious cleanup. It's a mess right now.

--- src/motion/registration.cpp
+++ src/motion/registration.cpp
@@ -1,13 +1,16 @@
 #include "motion/registration.h"
 
+#include <algorithm>
 #include <utility>
 #include <vector>
 
 #include "image/image_data.h"
 #include "motion/motion_shift.h"
 
+#include "opencv2/calib3d/calib3d.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/features2d/features2d.hpp"
+#include "opencv2/video/tracking.hpp"
 #include "opencv2/highgui/highgui.hpp"  // TODO: Remove!
 
 #include "glog/logging.h"
@@ -15,6 +18,13 @@
 namespace super_resolution {
 namespace registration {
 namespace {
+
+constexpr double kFlannDistanceScalingFactor = 5.0;
+constexpr double kFlannDistanceThreshold = 0.04;
+constexpr double kRansacReprojectionThreshold = 0.1;
+
+using KeypointPairing =
+    std::pair<std::vector<cv::Point2f>, std::vector<cv::Point2f>>;
 
 // Stores OpenCV keypoints and their associated feature descriptors.
 struct KeypointsAndDescriptors {
@@ -48,16 +58,50 @@ KeypointsAndDescriptors DetectKeypoints(const ImageData& image) {
   return keypoints_and_descriptors;
 }
 
-// Computes pairwise matches between the two given keypoint sets.
-std::vector<std::pair<cv::Point2f, cv::Point2f>> FindMatchingFeatures(
+KeypointPairing ApplyRANSAC(const KeypointPairing& unfiltered_matches) {
+  if (unfiltered_matches.first.size() < 3) {
+    LOG(WARNING) << "Cannot apply RANSAC with less than 3 keypoint matches ("
+                 << unfiltered_matches.first.size() << " given).";
+    return unfiltered_matches;
+  }
+
+  CHECK_EQ(unfiltered_matches.first.size(), unfiltered_matches.second.size())
+      << "Imbalanced keypoint pairs. "
+      << "Number of matched keypoints must be the same across both images.";
+
+  std::vector<unsigned char> inliers_mask;
+  cv::findHomography(
+      unfiltered_matches.first,
+      unfiltered_matches.second,
+      CV_RANSAC,
+      kRansacReprojectionThreshold,
+      inliers_mask);
+
+  KeypointPairing filtered_matches;
+	for (int i = 0; i < inliers_mask.size(); ++i) {
+    if (inliers_mask[i] != static_cast<unsigned char>(0)) {
+      filtered_matches.first.push_back(unfiltered_matches.first[i]);
+      filtered_matches.second.push_back(unfiltered_matches.second[i]);
+    }
+  }
+  LOG(INFO) << "Filtered from " << unfiltered_matches.first.size()
+            << " to " << filtered_matches.first.size() << " matches.";
+  return filtered_matches;
+}
+
+// Computes pairwise keypoint matches between the two given feature descriptor
+// sets. This does does not guarantee ideal matches. Further filtering, such as
+// RANSAC, may be necessary.
+KeypointPairing FindMatchingFeatures(
     const KeypointsAndDescriptors& keypoints_and_descriptors_1,
     const KeypointsAndDescriptors& keypoints_and_descriptors_2) {
 
   // If there are no features available for one of the images, returns an empty
   // set of matches.
+  KeypointPairing keypoint_matches;
   if (keypoints_and_descriptors_1.descriptors.empty() ||
       keypoints_and_descriptors_2.descriptors.empty()) {
-    return {};  // Default empty list.
+    return keypoint_matches;
   }
 
   // Need to convert to CV_32F so that the OpenCV Flann-based matcher can work.
@@ -71,19 +115,39 @@ std::vector<std::pair<cv::Point2f, cv::Point2f>> FindMatchingFeatures(
   std::vector<cv::DMatch> feature_matches;
   matcher.match(descriptors_1, descriptors_2, feature_matches);
 
+  // Filter out the keypoint matches to only keep the best ones based on
+  // feature-space distance thresholding.
+  const int num_matches = feature_matches.size();
+  double smallest_feature_distance = 0.0;
+  if (num_matches > 0) {
+    smallest_feature_distance = feature_matches[0].distance;
+    for (int i = 1; i < num_matches; ++i) {
+      const double match_distance = feature_matches[i].distance;
+      smallest_feature_distance =
+          std::min(smallest_feature_distance, match_distance);
+    }
+  }
+  std::vector<cv::DMatch> good_feature_matches;
+  const double distance_threshold = std::max(
+      kFlannDistanceScalingFactor * smallest_feature_distance,
+      kFlannDistanceThreshold);
+  for(const cv::DMatch& match : feature_matches) {
+    if (match.distance <= distance_threshold) {
+      good_feature_matches.push_back(match);
+    }
+  }
+
   // Build a list of keypoint match pairs.
-  std::vector<std::pair<cv::Point2f, cv::Point2f>> all_keypoint_matches;
-  for (const cv::DMatch& match : feature_matches) {
+  for (const cv::DMatch& match : good_feature_matches) {
     cv::Point2f pixel_loc_1 =
         keypoints_and_descriptors_1.keypoints[match.queryIdx].pt;
     cv::Point2f pixel_loc_2 =
         keypoints_and_descriptors_2.keypoints[match.trainIdx].pt;
-    all_keypoint_matches.push_back(std::make_pair(pixel_loc_1, pixel_loc_2));
+    keypoint_matches.first.push_back(pixel_loc_1);
+    keypoint_matches.second.push_back(pixel_loc_2);
   }
 
-  // TODO: Filter out the keypoint matches and only keep the "good" ones.
-
-  return all_keypoint_matches;
+  return keypoint_matches;
 }
 
 }  // namespace
@@ -91,39 +155,76 @@ std::vector<std::pair<cv::Point2f, cv::Point2f>> FindMatchingFeatures(
 MotionShiftSequence TranslationalRegistration(
     const std::vector<ImageData>& images) {
 
-  std::vector<MotionShift> motion_shifts;
-  const int num_images = images.size();
-  if (num_images == 1) {
-    motion_shifts.push_back(MotionShift(0, 0));
-  } else if (num_images > 1) {
-    const KeypointsAndDescriptors& image_0_keypoints =
-        DetectKeypoints(images[0]);
-    for (int i = 1; i < num_images; ++i) {
-      const KeypointsAndDescriptors& image_i_keypoints =
-          DetectKeypoints(images[i]);
-      const auto& keypoint_matches = FindMatchingFeatures(
-          image_0_keypoints, image_i_keypoints);
+  // If no images, return an empty sequence.
+  if (images.empty()) {
+    LOG(WARNING) << "No images given. Returning an empty motion sequence.";
+    return MotionShiftSequence();
+  }
 
-      // TODO: Remove! Replace this with the motion computation (homography).
-      cv::Mat vis;
-      cv::hconcat(
-          images[0].GetVisualizationImage(),
-          images[i].GetVisualizationImage(),
-          vis);
-      const cv::Scalar vis_line_color(0, 255, 0);
-      for (const auto& keypoint_pair : keypoint_matches) {
-        cv::line(
-          vis,
-          keypoint_pair.first,
-          keypoint_pair.second + cv::Point2f(images[i].GetImageSize().width, 0),
-          vis_line_color,
-          1,  // Line thickness.
-          CV_AA);
-      }
-      cv::imshow("Keypoint Matches", vis);
-      cv::waitKey(0);
-      // ----
-    }
+  // The first image is relative to itself, so its shift is always (0, 0).
+  std::vector<MotionShift> motion_shifts;
+  motion_shifts.push_back(MotionShift(0, 0));
+
+  // Run keypoint matching between the first image and all other images.
+  const KeypointsAndDescriptors& image_0_keypoints =
+      DetectKeypoints(images[0]);
+  const int num_images = images.size();
+  for (int i = 1; i < num_images; ++i) {
+    const KeypointsAndDescriptors& image_i_keypoints =
+        DetectKeypoints(images[i]);
+    const KeypointPairing& keypoint_matches =
+        FindMatchingFeatures(image_0_keypoints, image_i_keypoints);
+    const KeypointPairing& good_matches = ApplyRANSAC(keypoint_matches);
+    //const cv::Mat homography = ComputeHomography(keypoint_matches);
+    //LOG(INFO) << "\n" << homography;
+    //cv::Mat intrinsics = cv::Mat::ones(cv::Size(3, 3), CV_32F);
+    //std::vector<cv::Mat> ignored_rotations, ignored_normals;
+    //std::vector<cv::Mat> translations;
+    //cv::decomposeHomographyMat(
+    //    homography,
+    //    intrinsics,
+    //    ignored_rotations,
+    //    translations,
+    //    ignored_normals);
+    //LOG(INFO) << "-----";
+    //for (const cv::Mat t : translations) {
+    //  const double x = t.at<float>(0) / t.at<float>(2);
+    //  const double y = t.at<float>(1) / t.at<float>(2);
+    //  LOG(INFO) << "T: " << x << ", " << y;
+    //}
+    //LOG(INFO) << "\n\n";
+
+    //const cv::Mat affine_transform = cv::estimateRigidTransform(
+    //    keypoint_matches.first, keypoint_matches.second, true);
+    const cv::Mat affine_transform = cv::estimateRigidTransform(
+        good_matches.first, good_matches.second, true);
+    CHECK(!affine_transform.empty())
+        << "Could not determine motion shift between images.";
+    const double dx = affine_transform.at<double>(0, 2);
+    const double dy = affine_transform.at<double>(1, 2);
+    LOG(INFO) << "\n" << affine_transform;
+    LOG(INFO) << "T(x, y) = " << dx << ", " << dy;
+    motion_shifts.push_back(MotionShift(dx, dy));
+
+//    // TODO: Remove! Replace this with the motion computation (homography).
+//    cv::Mat vis;
+//    cv::hconcat(
+//        images[0].GetVisualizationImage(),
+//        images[i].GetVisualizationImage(),
+//        vis);
+//    const cv::Scalar vis_line_color(0, 255, 0);
+//    for (int i = 0; i < good_matches.first.size(); ++i) {
+//      cv::line(
+//        vis,
+//        good_matches.first[i],
+//        good_matches.second[i] + cv::Point2f(images[i].GetImageSize().width, 0),
+//        vis_line_color,
+//        1,  // Line thickness.
+//        CV_AA);
+//    }
+//    cv::imshow("Keypoint Matches", vis);
+//    cv::waitKey(0);
+//    // ----
   }
   return MotionShiftSequence(motion_shifts);
 }
---
+++ test/test_registration.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <vector>
+
+#include "image/image_data.h"
+#include "image_model/motion_module.h"
+#include "motion/registration.h"
+#include "util/data_loader.h"
+#include "util/util.h"
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+using super_resolution::ImageData;
+using super_resolution::MotionShift;
+using super_resolution::MotionShiftSequence;
+
+constexpr double kTranslationEstimateErrorTolerance = 0.01;
+
+static const std::string kTestImagePath =
+    super_resolution::util::GetAbsoluteCodePath("test_data/goat.jpg");
+
+// Tests that the peak signal to noise ratio evaluator gives the correct scores.
+TEST(Registration, TranslationalRegistration) {
+  std::vector<MotionShift> ground_truth_shifts({
+    MotionShift(0, 0),
+    MotionShift(0, 1),
+    MotionShift(2, 0),
+    MotionShift(5, 5),
+    MotionShift(-5, -1)
+  });
+  const MotionShiftSequence ground_truth_sequence(ground_truth_shifts);
+
+  // Load the original image and apply motion shifts to it.
+  const ImageData original_image =
+      super_resolution::util::LoadImage(kTestImagePath);
+  const super_resolution::MotionModule motion_module(ground_truth_sequence);
+  std::vector<ImageData> shifted_images;
+  const int num_motion_shifts = ground_truth_sequence.GetNumMotionShifts();
+  for (int i = 0; i < num_motion_shifts; ++i) {
+    ImageData shifted_image = original_image;
+    motion_module.ApplyToImage(&shifted_image, i);
+    shifted_images.push_back(shifted_image);
+  }
+
+  // Try to register it and test that the registered results are close to the
+  // ground truth.
+  const MotionShiftSequence registered_sequence =
+      super_resolution::registration::TranslationalRegistration(shifted_images);
+  EXPECT_EQ(registered_sequence.GetNumMotionShifts(), num_motion_shifts);
+  for (int i = 0; i < num_motion_shifts; ++i) {
+    const MotionShift ground_truth_shift = ground_truth_sequence[i];
+    const MotionShift estimated_shift = registered_sequence[i];
+    EXPECT_NEAR(
+        ground_truth_shift.dx,
+        estimated_shift.dx,
+        kTranslationEstimateErrorTolerance);
+    EXPECT_NEAR(
+        ground_truth_shift.dy,
+        estimated_shift.dy,
+        kTranslationEstimateErrorTolerance);
+
+    std::cout << ground_truth_shift.dx << ", " << ground_truth_shift.dy
+              << " vs. "
+              << estimated_shift.dx << ", " << estimated_shift.dy << std::endl;
+  }
+}
