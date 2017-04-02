de3f96bea47976a47d63193c829cdd4b9a4d30d7
Richard Teammco <teammcr192@gmail.com>
2017-04-02 16:17:35 -0500

    Added a basic zoom-in for visualization window.
    User can drag a rectangle on the screen which zooms in to the selection.
    Currently not implemented well enough. Needs to be finished and tested.

--- src/util/visualization.cpp
+++ src/util/visualization.cpp
@@ -21,6 +21,74 @@ namespace {
 constexpr int kDisplayWidthPixels = 1250;
 constexpr int kDisplayHeightPixels = 850;
 
+// A state for the OpenCV window mouse callback that allows tracking dragging
+// and rectangle position over time.
+struct WindowInteractionStatus {
+  WindowInteractionStatus(const cv::Mat image, const std::string window_name)
+      : original_image(image), window_name(window_name) {}
+
+  const cv::Mat original_image;
+  const std::string window_name;
+
+  int drag_start_x = 0;
+  int drag_start_y = 0;
+  bool dragging = false;
+};
+
+// Callback function for OpenCV's window. This implements logic that allows the
+// user to zoom in to sections of the image by drawing rectangles.
+void DisplayWindowMouseCallback(
+    const int event,
+    const int x_pos,
+    const int y_pos,
+    const int flags,
+    void *ptr) {
+
+  WindowInteractionStatus* status =
+      reinterpret_cast<WindowInteractionStatus*>(ptr);
+
+  // If left button is pressed, start dragging.
+  if (event == CV_EVENT_LBUTTONDOWN) {
+    status->drag_start_x = x_pos;
+    status->drag_start_y = y_pos;
+    status->dragging = true;
+  }
+
+  // Dragging was interrupted if the status is set to dragging but the left
+  // button is no longer being pushed down. This can happen if the mouse goes
+  // off screen. In this case, cancel the drag-to-zoom-in operation.
+  if (status->dragging && !(flags & CV_EVENT_FLAG_LBUTTON)) {
+    cv::imshow(status->window_name, status->original_image);
+    status->dragging = false;
+  }
+
+  // If left button is released during dragging, perform the zoom.
+  if (event == CV_EVENT_LBUTTONUP && status->dragging) {
+    const int top_y = std::min(y_pos, status->drag_start_y);
+    const int left_x = std::min(x_pos, status->drag_start_x);
+    const int selection_width = std::abs(x_pos - status->drag_start_x);
+    const int selection_height = std::abs(y_pos - status->drag_start_y);
+    const cv::Rect selection(top_y, left_x, selection_width, selection_height);
+    cv::Mat cropped_image = status->original_image(selection);
+    cv::resize(cropped_image, cropped_image, status->original_image.size());
+    cv::imshow(status->window_name, cropped_image);
+    status->dragging = false;
+  } else if (status->dragging) {
+    // If dragging, draw a rectangle to indicate the user's current selection.
+    // TODO: Adjust the line color and parameters with constant variables.
+    cv::Mat selection_image = status->original_image.clone();
+    cv::rectangle(
+        selection_image,
+        cv::Point(status->drag_start_x, status->drag_start_y),
+        cv::Point(x_pos, y_pos),
+        cv::Scalar(0, 255, 255),  // color
+        1,  // thickness
+        8,  // lineType
+        0);  // shift
+    cv::imshow(status->window_name, selection_image);
+  }
+}
+
 }  // namespace
 
 void DisplayImage(
@@ -28,7 +96,7 @@ void DisplayImage(
     const std::string& window_name,
     const bool rescale) {
 
-  ImageData display_image = image;
+  ImageData display_image = ImageData(image.GetVisualizationImage());
   const cv::Size image_size = display_image.GetImageSize();
   if (rescale) {
     const double scale_x =
@@ -41,8 +109,12 @@ void DisplayImage(
     display_image.ResizeImage(scale);
   }
 
-  cv::namedWindow(window_name, CV_WINDOW_AUTOSIZE);
-  cv::imshow(window_name, display_image.GetVisualizationImage());
+  //cv::namedWindow(window_name, CV_WINDOW_AUTOSIZE);
+  cv::namedWindow(window_name, CV_WINDOW_NORMAL);
+  WindowInteractionStatus status(
+      display_image.GetVisualizationImage(), window_name);
+  cv::setMouseCallback(window_name, DisplayWindowMouseCallback, &status);
+  cv::imshow(window_name, status.original_image);
   std::cout << "Displaying image. Press any key to continue." << std::endl;
   cv::waitKey(0);
   cv::destroyWindow(window_name);
